57f04f55472825b76356fda11405406a0aef8463
Aubrey Yan <=>
2023-04-15 04:15:36 -0400

    more functionality checkboxes and curve

--- src/pages/CS4510.tsx
+++ src/pages/CS4510.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import {isMobile} from 'react-device-detect';
 import {
     Alert, Box,
-    Button,
+    Button, Checkbox, FormControlLabel, FormGroup,
     Snackbar, TextField,
     Typography
 } from "@mui/material";
@@ -56,11 +56,13 @@ const WEIGHTS: {
     "proj": 0.1,
 };
 
+const OUTDATED_COOKIES = ["cs4510-sp2023-grade-store"];
+
 
 function CS4510() {
     const [displayInfo, setDisplayInfo] = useState("");
     const [finalGrade, setFinalGrade] = useState(0);
-    const cacheKey = "cs4510-sp2023-grade-store";
+    const cacheKey = "cs4510-sp2023-grade-store-1";
     const [cache, setCache] = useState(JSON.parse(localStorage.getItem(cacheKey) || "{}") || null);
     const [mobileAlert, setMobileAlert] = useState(isMobile);
     const [exam1, setExam1] = useState("0");
@@ -73,7 +75,15 @@ function CS4510() {
     const [hw4, setHw4] = useState("0");
     const [hw5, setHw5] = useState("0");
     const [hw6, setHw6] = useState("0");
+    const [hw1Bonus, setHw1Bonus] = useState(false);
+    const [hw2Bonus, setHw2Bonus] = useState(false);
+    const [hw3Bonus, setHw3Bonus] = useState(false);
+    const [hw4Bonus, setHw4Bonus] = useState(false);
+    const [hw5Bonus, setHw5Bonus] = useState(false);
+    const [hw6Bonus, setHw6Bonus] = useState(false);
     const [proj, setProj] = useState("0");
+    const [curve, setCurve] = useState("0");
+    OUTDATED_COOKIES.forEach((cookie) => localStorage.removeItem(cookie));
 
     document.title = "cs4510sp2023"
 
@@ -147,10 +157,11 @@ function CS4510() {
     function updateGrades() {
         let exams: number[] = [];
         let hws: number[] = [];
+        let hwBonuses: boolean[] = [hw1Bonus, hw2Bonus, hw3Bonus, hw4Bonus, hw5Bonus, hw6Bonus];
         let projs: number[] = [];
         Object.keys(TOTAL_POINTS).forEach((grade) => {
             let ref = acquireRef(grade);
-            const score = Number(ref) / TOTAL_POINTS[grade] * WEIGHTS[grade] * 100;
+            let score = Number(ref) / TOTAL_POINTS[grade] * WEIGHTS[grade] * 100;
             if (grade.startsWith("exam")) {
                 if (exams.length < 3) {
                     exams.push(score)
@@ -162,6 +173,7 @@ function CS4510() {
                     }
                 }
             } else if (grade.startsWith("hw")) {
+                score = score * (hwBonuses[0] ? 1.05 : 1)
                 if (hws.length < 5) {
                     hws.push(score)
                 } else {
@@ -171,11 +183,13 @@ function CS4510() {
                         hws.push(score);
                     }
                 }
+                hwBonuses.splice(0, 1);
             } else {
                 projs.push(score);
             }
         });
-        setFinalGrade([exams, hws, projs].map((category) => category.reduce((partialSum, a) => partialSum + a, 0)).reduce((partialSum, a) => partialSum + a, 0));
+        const finalGrade = [exams, hws, projs].map((category) => category.reduce((partialSum, a) => partialSum + a, 0)).reduce((partialSum, a) => partialSum + a, 0);
+        setFinalGrade(finalGrade + Number(curve));
     }
 
     function handleClick() {
@@ -185,13 +199,13 @@ function CS4510() {
 
     function handleCookie() {
         // @ts-ignore
-        localStorage.setItem(cacheKey, JSON.stringify([exam1, exam2, exam3, exam4, hw1, hw2, hw3, hw4, hw5, hw6, proj]))
+        localStorage.setItem(cacheKey, JSON.stringify([exam1, exam2, exam3, exam4, hw1, hw2, hw3, hw4, hw5, hw6, proj, curve, hw1Bonus, hw2Bonus, hw3Bonus, hw4Bonus, hw5Bonus, hw6Bonus]))
         setDisplayInfo("Your grades have been saved locally as cookies!");
     }
 
     useEffect(() => {
-        if (cache != null && cache.length > 2) {
-            const [exam1CACHE, exam2CACHE, exam3CACHE, exam4CACHE, hw1CACHE, hw2CACHE, hw3CACHE, hw4CACHE, hw5CACHE, hw6CACHE, projCACHE] = cache;
+        if (cache != null && cache.length > 0) {
+            const [exam1CACHE, exam2CACHE, exam3CACHE, exam4CACHE, hw1CACHE, hw2CACHE, hw3CACHE, hw4CACHE, hw5CACHE, hw6CACHE, projCACHE, curveCACHE, hw1BonusCACHE, hw2BonusCACHE, hw3BonusCACHE, hw4BonusCACHE, hw5BonusCACHE, hw6BonusCACHE] = cache;
             setExam1(exam1CACHE);
             setExam2(exam2CACHE);
             setExam3(exam3CACHE);
@@ -203,6 +217,13 @@ function CS4510() {
             setHw5(hw5CACHE);
             setHw6(hw6CACHE);
             setProj(projCACHE);
+            setHw1Bonus(hw1BonusCACHE);
+            setHw2Bonus(hw2BonusCACHE);
+            setHw3Bonus(hw3BonusCACHE);
+            setHw4Bonus(hw4BonusCACHE);
+            setHw5Bonus(hw5BonusCACHE);
+            setHw6Bonus(hw6BonusCACHE);
+            setCurve(curveCACHE);
         }
         updateGrades();
     }, [])
@@ -244,7 +265,7 @@ function CS4510() {
                 </Box>
 
                 <Box style={{ padding: '20px'}}>
-                    <Typography variant="h6" style={{position: "absolute"}}>Homeworks (45%) (Lowest Dropped)</Typography>
+                    <Typography variant="h6" style={{position: "absolute"}}>Homeworks (45%) (Lowest Dropped) (LaTex Bonus 5% Per Homework)</Typography>
                 </Box>
 
                 <Box component="form" autoComplete="off" style={{ padding: '20px'}}>
@@ -277,6 +298,17 @@ function CS4510() {
                     ])}
                 </Box>
 
+                <Box component="form" autoComplete="off" style={{ padding: '20px'}}>
+                    <FormGroup >
+                        <FormControlLabel control={<Checkbox checked={hw1Bonus} onClick={() => setHw1Bonus(!hw1Bonus)} />} label={<Typography variant="h6" style={{position: "relative"}}>HW1 LaTex+</Typography>} />
+                        <FormControlLabel control={<Checkbox checked={hw2Bonus} onClick={() => setHw2Bonus(!hw2Bonus)} />} label={<Typography variant="h6" style={{position: "relative"}}>HW2 LaTex+</Typography>} />
+                        <FormControlLabel control={<Checkbox checked={hw3Bonus} onClick={() => setHw3Bonus(!hw3Bonus)} />} label={<Typography variant="h6" style={{position: "relative"}}>HW3 LaTex+</Typography>} />
+                        <FormControlLabel control={<Checkbox checked={hw4Bonus} onClick={() => setHw4Bonus(!hw4Bonus)} />} label={<Typography variant="h6" style={{position: "relative"}}>HW4 LaTex+</Typography>} />
+                        <FormControlLabel control={<Checkbox checked={hw5Bonus} onClick={() => setHw5Bonus(!hw5Bonus)} />} label={<Typography variant="h6" style={{position: "relative"}}>HW5 LaTex+</Typography>} />
+                        <FormControlLabel control={<Checkbox checked={hw6Bonus} onClick={() => setHw6Bonus(!hw6Bonus)} />} label={<Typography variant="h6" style={{position: "relative"}}>HW6 LaTex+</Typography>} />
+                    </FormGroup>
+                </Box>
+
                 <Box style={{ padding: '20px'}}>
                     <Typography variant="h6" style={{position: "absolute"}}>Final Project (10%)</Typography>
                 </Box>
@@ -291,6 +323,14 @@ function CS4510() {
                     ])}
                 </Box>
 
+                <Box style={{ padding: '20px'}}>
+                    <Typography variant="h6" style={{position: "absolute"}}>Miscellaneous Curve (CIOS) (%)</Typography>
+                </Box>
+
+                <Box component="form" autoComplete="off" style={{ padding: '20px'}}>
+                    <TextField variant="filled" key="curve" id="curve" label={`Curve (1 = 1%)`} value={curve} onChange={(event) => setCurve(event.target.value)} />
+                </Box>
+
                 <span style={{ position: 'absolute', padding: '10px' }}>
                     <Button variant="contained" onClick={handleClick}>Submit</Button>
                 </span>
@@ -298,10 +338,10 @@ function CS4510() {
                     <Button variant="outlined" onClick={handleCookie}>Save Grades Locally</Button>
                 </span>
                 <Typography variant="h2" style={{ position: 'absolute', padding: '20px', marginTop: '50px' }}>{`Your final grade is ${finalGrade.toFixed(3)}% (${
-                    finalGrade >= 90 ? "A" :
-                        finalGrade >= 80 ? "B" :
-                            finalGrade >= 70 ? "C" :
-                                finalGrade >= 65 ? "D" : "F"
+                    Math.round(finalGrade) >= 90 ? "A" :
+                        Math.round(finalGrade) >= 80 ? "B" :
+                            Math.round(finalGrade) >= 70 ? "C" :
+                                Math.round(finalGrade) >= 65 ? "D" : "F"
                 })!`}</Typography>
                 <Snackbar
                     open={displayInfo !== ""}
